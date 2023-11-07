41e3b6e2ae6e5e05b3f1666a53ec878e716845fd
emrvb <emrverskn@gmail.com>
2023-11-07 21:46:27 +0300

    modified

--- src/cblog/settings.py
+++ src/cblog/settings.py
@@ -88,7 +88,7 @@ DATABASES = {
         'NAME': 'merve', # database name in RDS is written here
         'USER': 'merve', # database master username in RDS is written here
         'PASSWORD': '123456789',
-        'HOST': 'merve.cmzrqmrggkmg.us-east-1.rds.amazonaws.com', # database endpoint is written here
+        'HOST': 'mrv.cmzrqmrggkmg.us-east-1.rds.amazonaws.com', # database endpoint is written here
         'PORT': '3306' # database port is written here
     }
 }
